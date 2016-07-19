f978c614fb424a8759f5e47b433888edb8b9809f
Nikolay <halt.hammerzeit.at@gmail.com>
2016-07-19 23:17:19 +0300

    Fixing put and patch

--- source/http.js
+++ source/http.js
@@ -34,13 +34,25 @@ for (let method of Object.keys(http_methods))
 
 			if (data)
 			{
-				if (http_method === 'post')
+				switch (http_method)
 				{
-					request.send(data)
-				}
-				else
-				{
-					request.query(data)
+					case 'get':
+						request.query(data)
+						break
+
+					case 'post':
+					case 'put':
+					case 'patch':
+					case 'head':
+					case 'options':
+						request.send(data)
+						break
+
+					case 'delete':
+						throw new Error(`"data" supplied for HTTP DELETE request: ${JSON.stringify(data)}`)
+
+					default:
+						throw new Error(`Unknown HTTP method: ${http_method}`)
 				}
 			}
 
