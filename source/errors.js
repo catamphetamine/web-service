89eb52141db815e24082f76eabe7940a6602a720
Nikolay <halt.hammerzeit.at@gmail.com>
2016-08-17 02:37:54 +0300

    better custom errors

--- source/errors.js
+++ source/errors.js
@@ -1,68 +1,53 @@
-import { exists } from './helpers'
+import { exists, is_object } from './helpers'
 
 export default
 {
-	Input_missing          : custom_error('Missing input',          { additional_properties: { http_status_code: 400 } }),
-	Unauthenticated        : custom_error('Unauthenticated',        { additional_properties: { http_status_code: 401 } }),
-	Unauthorized           : custom_error('Unauthorized',           { additional_properties: { http_status_code: 403 } }),
-	Access_denied          : custom_error('Access denied',          { additional_properties: { http_status_code: 403 } }),
-	Not_found              : custom_error('Not found',              { additional_properties: { http_status_code: 404 } }),
-	Conflict               : custom_error('Conflict',               { additional_properties: { http_status_code: 409 } }),
-	Unsupported_input_type : custom_error('Unsupported input type', { additional_properties: { http_status_code: 415 } }),
-	Input_rejected         : custom_error('Input rejected',         { additional_properties: { http_status_code: 422 } }),
-	Error                  : custom_error('Server error',           { additional_properties: { http_status_code: 500 } })
+	Input_missing          : custom_error('Missing input',          { http_status_code: 400 }),
+	Unauthenticated        : custom_error('Unauthenticated',        { http_status_code: 401 }),
+	Unauthorized           : custom_error('Unauthorized',           { http_status_code: 403 }),
+	Access_denied          : custom_error('Access denied',          { http_status_code: 403 }),
+	Not_found              : custom_error('Not found',              { http_status_code: 404 }),
+	Conflict               : custom_error('Conflict',               { http_status_code: 409 }),
+	Unsupported_input_type : custom_error('Unsupported input type', { http_status_code: 415 }),
+	Input_rejected         : custom_error('Input rejected',         { http_status_code: 422 }),
+	Error                  : custom_error('Server error',           { http_status_code: 500 })
 }
 
-function custom_error(name, { code, message, lock_message, additional_properties })
+function custom_error(name, default_properties)
 {
 	class Custom_error extends Error
 	{
-		constructor(argument)
+		constructor(message, properties)
 		{
 			super()
 
-			if (exists(code))
+			this.name    = name
+			this.message = name
+
+			if (is_object(message))
 			{
-				this.code = code
+				properties = message
+				message = undefined
 			}
 
-			if (exists(message))
+			if (message)
 			{
 				this.message = message
 			}
-			else
-			{
-				this.message = name
-			}
 
-			if (exists(argument))
+			if (exists(properties))
 			{
-				if (exists(argument.code))
+				for (let key of Object.keys(properties))
 				{
-					this.code = argument.code
-				}
-				
-				if (exists(argument.message))
-				{
-					this.message = argument.message
-				}
-
-				if (lock_message !== true)
-				{
-					this.message = argument || this.message
+					this[key] = properties[key]
 				}
 			}
 
-			this.name = name
-
-			if (additional_properties)
+			for (let key of Object.keys(default_properties))
 			{
-				for (let key of Object.keys(additional_properties))
+				if (this[key] === undefined)
 				{
-					if (this[key] === undefined)
-					{
-						this[key] = additional_properties[key]
-					}
+					this[key] = default_properties[key]
 				}
 			}
 
